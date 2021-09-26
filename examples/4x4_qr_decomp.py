4f4e668c7f93e1c14025abc7b67c82fbd52c100a
hsjoihs <hsjoihs@stanford.edu>
2021-09-26 05:36:57 +0100

    Finish doing Gram-Schmidt

--- examples/4x4_qr_decomp.py
+++ examples/4x4_qr_decomp.py
@@ -140,8 +140,75 @@ def hello_world(asm):
     # Finish computing u3!!!!
     """
 
+    """
+    Next, compute u4.
+    Note:
+    ra0: [...(u1 / u1.dot(u1)), ...0vec, ...0vec, ...0vec]
+    ra1: [...0vec, ...(u2 / u2.dot(u2)), ...0vec, ...0vec]
+    """
+
+    mov(r1, 0.0)
+    ldi(null,[0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1],set_flags=True)
+    mov(r1, r0, cond='zs') 
+    nop(); rotate(r2, r1, 12) # r2 has [...0vec, ...0vec, ...0vec, ...u1]
+    fmul(r2, r2, r0) 
+    # r2 has [...0vec, ...0vec, ...0vec, ...(u1*a4)]
+    nop(); rotate(r3, r2, 14); fadd(r3, r3, r2); nop(); rotate(r2, r3, 15); fadd(r2, r3, r2); nop(); rotate(r2, r2, 4); mov(broadcast, r2)
+    # r5 has u1.dot(a4)
+    mov(r1, ra0); fmul(r1, r1, r5)
+    # r1 has [...(u1 * u1.dot(a4) / u1.dot(u1)), ...0vec, ...0vec, ...0vec] 
+    nop(); rotate(r1, r1, 12); fsub(r0, r0, r1)
+    # r0: [...u1, ...u2, ...u3, ...t4] where t4 = a4 - (u1 * u1.dot(a4) / u1.dot(u1))
+
+    mov(r1, 0.0)
+    ldi(null,[1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1],set_flags=True)
+    mov(r1, r0, cond='zs') 
+    nop(); rotate(r2, r1, 8) # r2 has [...0vec, ...0vec, ...0vec, ...u2]
+    fmul(r2, r2, r0) 
+    # r2 has [...0vec, ...0vec, ...0vec, ...(u2*t4)]
+    nop(); rotate(r3, r2, 14); fadd(r3, r3, r2); nop(); rotate(r2, r3, 15); fadd(r2, r3, r2); nop(); rotate(r2, r2, 4); mov(broadcast, r2)
+    # r5 has u2.dot(t4)
+    mov(r1, ra1); fmul(r1, r1, r5)
+    # r1 has [...0vec, ...(u2 * u2.dot(t4) / u2.dot(u2)), ...0vec, ...0vec] 
+    nop(); rotate(r1, r1, 8); fsub(r0, r0, r1)
+    # r0: [...u1, ...u2, ...u3, ...s4] where s4 = t4 - (u2 * u2.dot(t4) / u2.dot(u2))
+    
+    mov(r2, r0); fmul(r2, r2, r2); nop(); rotate(r3, r2, 14); fadd(r3, r3, r2)
+    nop(); rotate(r2, r3, 15); fadd(r2, r3, r2); 
+    # r2 has [u1.dot(u1), ?, ?, ?, u2.dot(u2), ?, ?, ?, u3.dot(u3), ?, ?, ?, s4.dot(s4), ?, ?, ?]
+    nop(); rotate(r2, r2, 8); mov(broadcast, r2) 
+    # r5 has u3.dot(u3)
+
+    mov(r1, 0.0)
+    ldi(null,[1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1],set_flags=True)
+    mov(r1, r0, cond='zs')
+    mov(sfu_recip, r5)
+    nop()
+    nop()
+    fmul(ra2, r1, r4) 
+    # STORE INTO ra2
+    # ra2 has [...0vec, ...0vec, ...(u3 / u3.dot(u3)), ...0vec]
+
+    mov(r1, 0.0)
+    ldi(null,[1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1],set_flags=True)
+    mov(r1, r0, cond='zs') 
+    nop(); rotate(r2, r1, 4) # r2 has [...0vec, ...0vec, ...0vec, ...u3]
+    fmul(r2, r2, r0) 
+    # r2 has [...0vec, ...0vec, ...0vec, ...(u3*s4)]
+    nop(); rotate(r3, r2, 14); fadd(r3, r3, r2); nop(); rotate(r2, r3, 15); fadd(r2, r3, r2); nop(); rotate(r2, r2, 4); mov(broadcast, r2)
+    # r5 has u3.dot(s4)
+    mov(r1, ra2); fmul(r1, r1, r5)
+    # r1 has [...0vec, ...0vec, ...(u3 * u3.dot(s4) / u3.dot(u3)), ...0vec] 
+    nop(); rotate(r1, r1, 4); fsub(r0, r0, r1)
+    # r0: [...u1, ...u2, ...u3, ...u4] where u4 = t4 - (u3 * u3.dot(s4) / u3.dot(u3))
+
+    """
+    DONE!!!
+    """
+
+
     mov(vpm, r0)
-    mov(vpm, r5)
+    mov(vpm, r1)
 
     # Store the resulting vectors from VPM to the host memory (address=uniforms[1])
     setup_dma_store(nrows=2)
