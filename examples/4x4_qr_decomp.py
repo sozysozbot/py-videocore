1f854fc1bfe1b8828159764db7ab217965084ff8
hsjoihs <hsjoihs@stanford.edu>
2021-09-26 04:32:23 +0100

    Finish u2!

--- examples/4x4_qr_decomp.py
+++ examples/4x4_qr_decomp.py
@@ -14,10 +14,70 @@ def hello_world(asm):
     setup_vpm_read(nrows=1)
     setup_vpm_write()
 
-    # Move a
     mov(r0, vpm)
+    # r0: [...a1, ...a2, ...a3, ...a4]
+    mov(r1, 0.0)
+    # r1: [...0vec, ...0vec, ...0vec, ...0vec]
+
+    """
+    Compute u2 = a2 - (u1 * u1.dot(a2) / u1.dot(u1)).
+    """
+
+    ldi(null,[0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1],set_flags=True)
+    mov(r1, r0, cond='zs') 
+    # r1: [...u1, ...0vec, ...0vec, ...0vec]
+
+    mov(r2, r1) 
+    # r2: [...u1, ...0vec, ...0vec, ...0vec]
+
+    # r2: u1=[A, B, C, D, ...]
+    fmul(r2, r2, r2) 
+    # r2: [A^2, B^2, C^2, D^2, ...]
+    nop(); rotate(r3, r2, 14)
+    # r2: [A^2, B^2, C^2, D^2, ...], r3: [C^2, D^2, 0, 0, ...]
+    fadd(r3, r3, r2)
+    # r3: [A^2 + C^2, B^2 + D^2, C^2, D^2, ...]
+    nop(); rotate(r2, r3, 15)
+    # r3: [A^2 + C^2, B^2 + D^2, ...], r2: : [B^2 + D^2, C^2, ...]
+    fadd(r2, r3, r2); 
+    # r2: [A^2+B^2+C^2+D^2, ...]
+    mov(broadcast, r2) 
+    # r5 has u1.dot(u1)
+
+    
+    nop(); rotate(r2, r1, 4) # r2 has [...0vec, ...u1, ...0vec, ...0vec]
+
+    mov(sfu_recip, r5)
+    nop()
+    nop()
+    fmul(r1, r1, r4) 
+    # r1 has [...(u1 / u1.dot(u1)), ...0vec, ...0vec, ...0vec]
+    fmul(r2, r2, r0) 
+    # r2 has [...0vec, ...(u1*a2), ...0vec, ...0vec]
+    # let r2 be [...0vec, A, B, C, D, ...0vec, ...0vec]
+    nop(); rotate(r3, r2, 14)
+    # r2: [0, 0, 0, 0, A, B, C, D, ...0vec, ...0vec]
+    # r3: [0, 0, A, B, C, D, 0, 0, ...0vec, ...0vec]
+    fadd(r3, r3, r2)
+    # r3: [0, 0, A, B, A+C, B+D, 0, 0, ...0vec, ...0vec]
+    nop(); rotate(r2, r3, 15); 
+    # r3: [0, 0, A, B, A+C, B+D, 0, 0, ...0vec, ...0vec]
+    # r2: [0, A, B, A+C, B+D, 0, 0, 0, ...0vec, ...0vec]
+    fadd(r2, r3, r2); 
+    # r2: [...?, u1.dot(a2), ...?, ...?]
+    nop(); rotate(r2, r2, 12); mov(broadcast, r2)
+    # r5 has u1.dot(a2)
+    
+    nop(); rotate(r1, r1, 4); fmul(r1, r1, r5)
+    # r1 has [...0vec, ...(u1 * u1.dot(a2) / u1.dot(u1)), ...0vec, ...0vec]
+
+    fsub(r0, r0, r1)
+    # r0: [...u1, ...u2, ...a3, ...a4]
+    """
+    Finish computing u2!!!!
+    """
     mov(vpm, r0)
-    mov(vpm, r0)
+    mov(vpm, 0.0)
 
     # Store the resulting vectors from VPM to the host memory (address=uniforms[1])
     setup_dma_store(nrows=2)
@@ -29,12 +89,11 @@ def hello_world(asm):
 
 with Driver() as drv:
     dim = 4
-    # Input vectors
-    a = np.random.random(dim * dim).astype('float32')
-    b = np.random.random(dim * dim).astype('float32')
+    # Input vector
+    a_vec = np.random.random(dim * dim).astype('float32')
 
     # Copy vectors to shared memory for DMA transfer
-    inp = drv.copy(np.r_[a])
+    inp = drv.copy(np.r_[a_vec])
     out = drv.alloc(dim * dim * 2, 'float32')
 
     # Run the program
@@ -44,14 +103,20 @@ with Driver() as drv:
             uniforms=[inp.address, out.address]
             )
 
-    a_mat = a.reshape((dim, dim))
+    a_mat = a_vec.reshape((dim, dim)).transpose()
     print(' A '.center(80, '='))
     print(a_mat)
+
+    (q_t, r_t) = out.reshape((2, dim, dim))
+    q = q_t.transpose()
+    r = r_t.transpose()
+
     print(' Q '.center(80, '='))
-    (q, r) = out.reshape((2, dim, dim))
     print(q)
     print(' R '.center(80, '='))
     print(r)
+    print(' Q^T @ Q '.center(80, '='))
+    print(q_t @ q)
     print(' error '.center(80, '='))
     print(np.abs(a_mat-(q@r)))
 
